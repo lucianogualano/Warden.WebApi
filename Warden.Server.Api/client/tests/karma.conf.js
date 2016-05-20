98a0c6e9d3123e5c24b8414b626ef3bb6c328815
Looch <luciano.gualano@yahoo.com>
2016-05-20 11:16:36 +1000

    added karma jasmine unit test

---
+++ Warden.Server.Api/client/tests/karma.conf.js
@@ -0,0 +1,109 @@
+﻿// Karma configuration
+// Generated on Mon May 09 2016 11:36:05 GMT+1000 (AUS Eastern Standard Time)
+
+module.exports = function (config) {
+    'use strict';
+
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '../',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            '../bower_components/angular/angular.js',
+            '../bower_components/angular-mocks/angular-mocks.js',
+            '../bower_components/angular-resource/angular-resource.js',
+            '../bower_components/angular-local-storage/dist/angular-local-storage.js',
+            '../bower_components/angular-google-places-autocomplete/dist/autocomplete.min.js',
+            '../bower_components/angular-route/angular-route.js',
+            '../bower_components/angular-ui-grid/ui-grid.js',
+            '../bower_components/jquery/dist/jquery.js',
+            '../bower_components/bootstrap/dist/js/bootstrap.js',            
+            '../bower_components/ngDialog/js/ngDialog.js',
+            '../bower_components/ngmap/build/scripts/ng-map.js',
+            '../bower_components/toastr/toastr.js',
+            'app/app.module.js',
+            'app/**/*.js',
+            'tests/unit/test.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [
+        ],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+           // 'src/**/*.html': ['ng-html2js'],
+            'src/**/!(*.mock|*.spec).js': ['coverage']
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'], //'htmlDetailed'
+
+        // notify karma of the available plugins
+        //plugins: [			   
+        //  'karma-jasmine',
+        //  'karma-phantomjs-launcher',
+        //  'karma-chrome-launcher',
+        //  'karma-ie-launcher',
+        //  'karma-html-detailed-reporter',
+        //  'karma-coverage'
+        //],
+
+        // Which plugins to enable
+        plugins: [
+          'karma-chrome-launcher',
+          'karma-phantomjs-launcher',
+          'karma-jasmine',
+          'karma-coverage'
+        ],
+
+        // configure the HTML-Detailed-Reporter to put all results in one file    
+        htmlDetailed: {
+            splitResults: true
+        },
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS', 'Chrome', 'IE'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        //concurrency: Infinity
+    })
+}
---
+++ Warden.Server.Api/client/tests/unit/test.js
@@ -0,0 +1,78 @@
+﻿'use strict';
+
+describe('Site CRUD service', function () {
+
+    var siteService;
+    var httpBackend;
+
+    // Given
+    var newSite;
+    var newSiteWithId;
+    var console = {
+        panel: $('body').append('<div>').css({ position: 'fixed', top: 0, right: 0, background: 'transpatent' }),
+        log: function (m) {
+            this.panel.prepend('<div>' + m + '</div>');
+        }
+
+    };
+    // Load app module
+    beforeEach(angular.mock.module('app'));
+
+    // Initialise dependencies, get service and mocked http backend
+    beforeEach(inject(function (_$httpBackend_, _SiteService_) {
+        siteService = _SiteService_;
+        httpBackend = _$httpBackend_;
+        newSite = {
+            Id: "00000000-0000-0000-0000-000000000000",
+            Name: 'george.smith@yahoo.com',
+            Address: '101 Collin St Melbourne 3000 VIC, Australia'
+        };
+        newSiteWithId =  {
+            Id: "399d0d7c-20ef-4e7f-84f7-fb83509c283f",
+            Name: 'george.smith@yahoo.com',
+            Address: '101 Collin St Melbourne 3000 VIC, Australia'
+        };
+        httpBackend.expectPOST('/api/sites/', newSite);
+        httpBackend.whenPOST('/api/sites/').respond(200, newSiteWithId);
+    }));
+
+    //describe('please', function () {
+    //    //it's 2014, not 1984 ... I think
+    //    it('should know 2 + 2 is not 5', function () {
+    //        expect(2).toEqual(2);
+    //    });
+    //});
+    // make sure no expectations were missed in the tests.
+    afterEach(function () {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
+
+    // Define test
+    it("should have 1 site in the controller view model repository", function () {
+        
+        //     When
+        expect(siteService).toBeDefined();
+        //siteService.test();
+        var returnedPromiseNewSite = siteService.insertSite(newSite);
+        //siteService.looch();
+
+        //    //Set up a handler for the response, that will put the result
+        //    // into a variable in this scope for you to test.
+        var result;
+        returnedPromiseNewSite.then(function (response) {
+            result = response;
+        });
+        
+        // Flush the backend to "execute" the request to do the expected POST assertion.
+        httpBackend.flush();
+
+        var templates = [JSON.stringify({ data: "data", name: "name" })];
+        //throw new Error(templates);
+        //throw new Error('Expected a spy, but got ' + JSON.stringify(result) + '.');
+        
+        // Then
+        expect(newSiteWithId).toEqual(result);
+    });
+
+});
